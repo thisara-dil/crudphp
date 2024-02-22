f1c110d429d6b47822f474a0607ec24700ebe542
S.A.T.D. Perara <136673697+thisara-dil@users.noreply.github.com>
2024-02-22 23:47:16 +0530

    Add files via upload

---
+++ crud/dbcon.php
@@ -0,0 +1,15 @@
+<?php
+define("HOSTNAME","localhost");
+define("USERNAME","root");
+define("PASSWORD","");
+define("DATABASE","crud_operation");
+
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+if(!$connection){
+    die("connection failed");
+}
+else{
+    echo "yes";
+}
+?>
